c18ad4fc57516cfa1024040fd73386ffdee04b86
Riya Gandhi <63222339+riya-gandhi@users.noreply.github.com>
2023-05-30 15:55:12 +0530

    Init: initialise repo

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require("path");
 const port = process.env.PORT || 1337;
 const app = express();
 
+const campaign = require("./controllers/admin/Weather");
+app.use("/weather", campaign);
+
 app.listen(port, () => {
   console.log("Server started at port: http://localhost:" + port);
 });
